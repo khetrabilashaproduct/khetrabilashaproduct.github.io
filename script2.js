3437d4032999f4be5412560375b31ae732e80184
khetrabilashaproduct <kbcp.work@gmail.com>
2025-12-03 08:42:26 +0530

    update webiste

---
+++ script2.js
@@ -0,0 +1,9 @@
+// Simple scroll notice
+window.addEventListener('load', () => {
+    console.log('Welcome to Khetra Bilasha!');
+  });
+// Simple scroll notice
+window.addEventListener('load', () => {
+    console.log('Welcome to Khetra Bilasha!');
+  });
+    
